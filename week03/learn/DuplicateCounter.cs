d7eadcf9a1d242e507958326cbcfc384694cc1b0
Jammonjones <wjssaan@gmail.com>
2023-11-15 14:14:06 -0700

    d

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,8 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var uniqueNums = new HashSet<int>(data);
+
+        return data.Length - uniqueNums.Count;
     }
 }
--- week03/learn/DuplicateCounterSolution.cs
+++ week03/learn/DuplicateCounterSolution.cs
@@ -21,7 +21,7 @@
 
         Console.WriteLine($"Number of items in the collection: {data.Length}");
         Console.WriteLine($"Number of duplicates: {CountDuplicates(data)}");
-        Console.WriteLine($"Number of duplicates (alternate): {CountDuplicatesAlternate(data)}");
+        Console.WriteLine($"Number of duplicates (alternate Solution): {CountDuplicatesAlternate(data)}");
     }
 
     /// <summary>
